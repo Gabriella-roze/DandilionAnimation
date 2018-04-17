8aaba737161f7173770d93f7787b86035e768d40
Gabriella G. R <g.rozenaite@gmail.com>
2018-04-17 22:46:07 +0200

    Big update
    Manu changes on background, clouds, flower "growing" into a sapling.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-// website loads
+let entrySound = document.getElementById('sound__entry');
+
+//Website loads, music starts, everything comes to places:
+
+entrySound.play();
+
+
+// website loads, after 3 sec:
+
+
+setTimeout(function(){
+  entrySound.play();
+}, 4500);

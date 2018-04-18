1d5c4efb6542724c9fa41a89a25965cb9911c55a
Gabriella G. R <g.rozenaite@gmail.com>
2018-04-18 18:14:43 +0200

    Nearly done version of first page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let btns = document.getElementById('buttons');
+let btnStart = document.getElementById('btn__start');
 let btnRain = document.getElementById('btn__rain');
 let btnSun = document.getElementById('btn__sun');
+let btnAlt = document.getElementById('btn__alt');
 let soundRain = document.getElementById('soundRain');
 let soundEntry = document.getElementById('sound__entry');
 let soundSun = document.getElementById('soundSun');
@@ -9,33 +11,67 @@ let cloud1 = document.getElementById('cloud1');
 let cloud2 = document.getElementById('cloud2');
 let cloud3 = document.getElementById('cloud3');
 
+// GENERAL
+btnStart.style.display = 'initial';
+btnRain.style.display = 'initial';
+btnSun.style.display = 'initial';
+btnAlt.style.display = 'initial';
+
+displayButtons();
+
+function displayButtons(){
+  if (btnStart.style.display == 'initial'){
+    btnRain.style.display = 'none';
+    btnSun.style.display = 'none';
+    btnAlt.style.display = 'none';
+  }
+  else if (btnRain.style.display == 'initial'){
+    btnStart.style.display = 'none';
+    btnSun.style.display = 'none';
+    btnAlt.style.display = 'none';
+  }
+  else if (btnSun.style.display == 'initial'){
+    btnStart.style.display = 'none';
+    btnRain.style.display = 'none';
+    btnAlt.style.display = 'none';
+  }
+  else {
+    btnStart.style.display = 'none';
+    btnRain.style.display = 'none';
+    btnSun.style.display = 'none';
+  }
+}
 
 
 //Website loads, music starts, everything comes to places:
 
-window.addEventListener("load", function(){
-  console.log("play");
+btnStart.addEventListener('click', function(){
   soundEntry.play();
-});
-
-// after 7s:
-
-setTimeout(function(){
-  audio.pause();
-}, 6900);
+  btnStart.style.display = 'none';
+  btnRain.style.display = 'initial';
+  displayButtons();
+})
 
 // when RAIN clicked, clouds, starts raining, plays raining sound:
 
 btnRain.addEventListener('click', function(){
+  soundEntry.volume = .3;
   soundRain.play();
   cloud1.classList.add("anim-shake");
   cloud2.classList.add("anim-shake");
   cloud3.classList.add("anim-shake");
   btnRain.style.display = 'none';
+  btnSun.style.display = 'initial';
+  btnRain.style.display = 'none';
+
   stopSound();
   stopAnim();
   on();
+  displayButtons();
 });
+function stopEntrySound(){
+    soundEntry.pause();
+  };
 function stopSound(){
   setTimeout(function(){
     soundRain.pause();
@@ -64,6 +100,8 @@ function off() {
 btnSun.addEventListener('click', function(){
   soundSun.play();
   onYellow();
+  btnSun.style.display = 'none';
+  btnAlt.style.display = 'initial';
   flower.src='images/flower3.png';
   btnSun.style.display = 'none';
   stopYellow();
